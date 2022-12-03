51dcf39d0ab31d3b8865def92fd5dec9ec11996a
lechurnair <75621929+lechurnair@users.noreply.github.com>
2022-12-04 00:25:39 +0530

    Update website.py

--- WebCode/website.py
+++ WebCode/website.py
@@ -21,17 +21,29 @@ def submit():
         body = {
             "query" : {
                 "match" : {
-                "content" : "science"
+                "content" : Q
     }
   },
   "size": 100
         }
+        body1 = {"suggest": {
+    "my-suggestion" : {
+      "text" : Q,
+      "term" : {
+        "field" : "content",
+        "suggest_mode" : "missing"
+        
+      }
+    }
+  }
+   }
         res = es.search(index="wikisearch1", body=body)
+        res1 = es.search(index = "wikisearch1", body = body1)
     else:
         print("error occured while making request")
-
+	
     #res1= [{'link':result['link'],'title':result['title']} for result in res['hits']['hits'][0]['_source']]
 
-    return render_template('result.html',result = res['hits']['hits'])
+    return render_template('result.html',result = res['hits']['hits'],suggest = res1['suggest']['my-suggestion'] )
 
 app.run(port=5000, debug=True)
